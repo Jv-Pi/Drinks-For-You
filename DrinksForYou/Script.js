3154bfdfe0885d854e354ab1d1234d9cc182f76b
João Victor Pires Dos Santos <54391360+Jv-Pi@users.noreply.github.com>
2021-01-12 00:31:52 +0400

    Update Script.js

--- DrinksForYou/Script.js
+++ DrinksForYou/Script.js
@@ -1,6 +1,24 @@
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-eventEmitter.emit('my_event');
-eventEmitter.on('my_event', () => {
-    console.log('data received successfully.');
-});
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle');
+    var banner = document.querySelector('.banner');
+    menuToggle.classList.toggle('active');
+    banner.classList.toggle('active');
+};
+
+window.onscroll = function() {
+    scroll();
+};
+
+function scroll() {
+    let btn = document.getElementById("btntop");
+    if (document.documentElement.scrollTop > 50) {
+        btn.style.display = "block"
+    } else {
+        btn.style.display = "none"
+    }
+};
+
+function up() {
+    document.documentElement.scrollTop = 0;
+
+}
